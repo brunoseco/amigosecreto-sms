17aa9589bd63fc459d3622dfc652db27f4ad0325
Bruno Seco - Target <bruno.seco@targetsoftware.com.br>
2022-12-18 17:23:00 -0300

    change zenvia to comtele sms sender

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,10 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Insira o número e nome dos participantes</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Insira um participante por linha, primeiro nome e depois telefone, separado por ;</h6>
-                            <textarea placeholder="Bruno;5515999999999"
-                                      v-model="textoParticipantes"
-                                      class="form-control"
-                                      @blur="calculaParticipantes"
-                                      rows="5" />
+                            <h6 class="card-subtitle mb-2 text-muted">Insira um participante por linha, primeiro nome e
+                                depois telefone, separado por ;</h6>
+                            <textarea placeholder="Bruno;5515999999999" v-model="textoParticipantes"
+                                class="form-control" @blur="calculaParticipantes" rows="5" />
                         </div>
                     </div>
 
@@ -28,10 +26,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Texto da mensagem</h5>
                             <h6 class="card-subtitle mb-2 text-muted">Insira um texto complementar</h6>
-                            <textarea placeholder="Amigo chocolate"
-                                      v-model="textoMensagem"
-                                      class="form-control"
-                                      rows="4" />
+                            <textarea placeholder="Amigo chocolate" v-model="textoMensagem" class="form-control"
+                                rows="4" />
                         </div>
                     </div>
                 </div>
@@ -48,7 +44,8 @@
                                     </div>
                                 </li>
                             </ol>
-                            <h6 class="card-subtitle mb-2 text-muted" v-else>Insira os participantes no passo anterior</h6>
+                            <h6 class="card-subtitle mb-2 text-muted" v-else>Insira os participantes no passo anterior
+                            </h6>
                         </div>
                     </div>
 
@@ -56,7 +53,9 @@
                         <div class="card-body">
                             <h5 class="card-title">Prévia da mensagem</h5>
                             <h6 class="card-subtitle mb-2 text-muted">
-                                Olá <strong>NOME 1</strong>, seu amigo(a) secreto(a) é <strong>NOME 2</strong>. {{ textoMensagem }}
+                                Olá <strong>NOME 1</strong>, seu amigo(a) secreto(a) é <strong>NOME 2</strong>. {{
+                                        textoMensagem
+                                }}
                             </h6>
                         </div>
                     </div>
@@ -65,7 +64,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Tudo pronto?</h5>
                             <h6 class="card-subtitle mb-2 text-muted">Confira todas as informações acima</h6>
-                            <button type="button" class="btn w-100 btn-success" @click="processar()">Processar e enviar</button>
+                            <button type="button" class="btn w-100 btn-success" @click="processar()">Processar e
+                                enviar</button>
                         </div>
                     </div>
                 </div>
@@ -95,106 +95,105 @@
 
 <script>
 
-    const { post } = require('request-promise');
+const { post } = require('request-promise');
 
-    export default {
-        name: 'App',
-        data() {
-            return {
-                textoParticipantes: null,
-                textoToken: null,
-                textoMensagem: null,
-                participantes: [],
-                participantesAtribuidos: [],
-                jsonEnvio: null
+export default {
+    name: 'App',
+    data() {
+        return {
+            textoParticipantes: null,
+            textoToken: null,
+            textoMensagem: null,
+            participantes: [],
+            participantesAtribuidos: [],
+            jsonEnvio: null
+        }
+    },
+    methods: {
+        calculaParticipantes() {
+            this.participantes = [];
+            let participantes = this.textoParticipantes.split("\n");
+            for (var i = 0; i < participantes.length; i++) {
+                const items = participantes[i].split(";");
+                if (items && items.length && items.length > 1) {
+                    const nome = items[0];
+                    const numero = items[1];
+                    this.participantes.push({
+                        nome: nome ? nome.trim().toUpperCase() : "",
+                        numero: nome ? numero.match(/\d+/g).join("") : ""
+                    });
+                }
             }
         },
-        methods: {
-            calculaParticipantes() {
-                this.participantes = [];
-                let participantes = this.textoParticipantes.split("\n");
-                for (var i = 0; i < participantes.length; i++) {
-                    const items = participantes[i].split(";");
-                    if (items && items.length && items.length > 1) {
-                        const nome = items[0];
-                        const numero = items[1];
-                        this.participantes.push({
-                            nome: nome ? nome.trim().toUpperCase() : "",
-                            numero: nome ? numero.match(/\d+/g).join("") : ""
-                        });
-                    }
-                }
-            },
 
-            embaralhaParticipantes() {
-                for (let i = this.participantes.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
-                    [this.participantes[i], this.participantes[j]] = [this.participantes[j], this.participantes[i]];
-                }
-            },
-
-            atribuiParticipantes() {
-                this.embaralhaParticipantes();
-                this.participantesAtribuidos = [];
-                for (let i = 0; i < this.participantes.length; i++) {
-                    const p1 = this.participantes[i];
-                    const p2 = this.participantes[i + 1] || this.participantes[0];
-                    this.participantesAtribuidos.push({ p1, p2 });
-                }
-            },
-
-            configuraEnvio() {
-                this.jsonEnvio = this.participantesAtribuidos.map((assign) => {
-                    return {
-                        nome: assign.p1.nome,
-                        numero: assign.p1.numero,
-                        status: "Pendente",
-                        mensagem: `Olá ${assign.p1.nome}, seu amigo(a) secreto(a) é ${assign.p2.nome}. ${this.textoMensagem}`,
-                    }
-                })
-            },
-
-            enviarApi() {
-
-                if (!this.textoToken)
-                    return;
-
-                this.jsonEnvio.forEach(item => {
-
-                    post({
-                        uri: 'https://api.zenvia.com/v2/channels/sms/messages',
-                        headers: {
-                            'X-API-TOKEN': this.textoToken
-                        },
-                        body: {
-                            from: '5515998027734',
-                            to: item.numero,
-                            contents: [{
-                                type: 'text',
-                                text: item.mensagem
-                            }]
-                        },
-                        json: true
-                    })
-                        .then(() => {
-                            item.status = "Sucesso";
-                        })
-                        .catch((error) => {
-                            item.status = "Erro, olhe a aba de console F12";
-                            console.log('Error:', error);
-                        });
-
-                });
-            },
-
-            processar() {
-                this.atribuiParticipantes();
-                this.configuraEnvio();
-                this.enviarApi();
+        embaralhaParticipantes() {
+            for (let i = this.participantes.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [this.participantes[i], this.participantes[j]] = [this.participantes[j], this.participantes[i]];
             }
+        },
+
+        atribuiParticipantes() {
+            this.embaralhaParticipantes();
+            this.participantesAtribuidos = [];
+            for (let i = 0; i < this.participantes.length; i++) {
+                const p1 = this.participantes[i];
+                const p2 = this.participantes[i + 1] || this.participantes[0];
+                this.participantesAtribuidos.push({ p1, p2 });
+            }
+        },
+
+        configuraEnvio() {
+            this.jsonEnvio = this.participantesAtribuidos.map((assign) => {
+                return {
+                    nome: assign.p1.nome,
+                    numero: assign.p1.numero,
+                    status: "Pendente",
+                    mensagem: `Olá ${assign.p1.nome}, seu amigo(a) secreto(a) é ${assign.p2.nome}. ${this.textoMensagem}`,
+                }
+            })
+        },
+
+        enviarApi() {
+
+            if (!this.textoToken) {
+                alert("TOKEN DA API NÃO ENVIADO")
+                return;
+            }
+
+            this.jsonEnvio.forEach(item => {
+                post({
+                    uri: 'https://sms.comtele.com.br/api/v2/send',
+                    headers: {
+                        'auth-key': this.textoToken
+                    },
+                    body: {
+                        sender: '5515998027734',
+                        receivers: item.numero,
+                        content: item.mensagem
+                    },
+                    json: true
+                })
+                    .then(() => {
+                        item.status = "Sucesso";
+                    })
+                    .catch((error) => {
+                        item.status = "Erro, olhe a aba de console F12";
+                        console.log('Error:', error);
+                    });
+
+            });
+        },
+
+        processar() {
+            this.atribuiParticipantes();
+            this.configuraEnvio();
+            this.enviarApi();
         }
     }
+}
 </script>
 
 <style>
+
 </style>
